990fae0bbefee18df40782e21ea10391123a6e39
Cameron Mkj <34096796+CameronMkj@users.noreply.github.com>
2018-01-09 13:33:36 +0000

    Update!
    I have updated the extention to feel less clunky. This automatically makes the user goto 'google.com' if they have visited the facebook page. Allowing the extention to feel a lot more fluid.

--- basics/content.js
+++ basics/content.js
@@ -1,21 +1,2 @@
 console.log("~You feel a presense watching over you...");
-userInput = prompt("Are you sure you want to procrastinate? (Y/N)");
-userInputTwo = userInput.toLowerCase();
-boolOne = false;
-while (!boolOne) {
-  if (userInputTwo == "y") {
-    userConfirm = confirm("Are you Sure?");
-    if (userConfirm == false) {
-      alert("Refreshing the page...")
-      location.reload();
-    } else if (userConfirm == true) {
-      boolOne = true;
-    }
-  } else if (userInputTwo == "n") {
-    alert("Redirecting you to Google.com...")
-    window.location.replace('https://www.google.com');
-    boolOne = true;
-  } else {
-    alert("That is not a valid option!");
-  }
-}
+window.location.replace('https://www.google.com');
